a13b65dc6687ad5c08ef2664fda69ae058a8ee28
Rob Bailey <rob.bailey3@gmail.com>
2021-02-24 20:55:16 +0000

    build: building the navigation

--- apps/site/src/app/homepage/homepage-root/homepage-root.component.spec.ts
+++ apps/site/src/app/homepage/homepage-root/homepage-root.component.spec.ts
@@ -1,18 +1,28 @@
 import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import * as e from 'express';
 import { HomepageBannerComponent } from '../homepage-banner/homepage-banner.component';
 
 import { HomepageRootComponent } from './homepage-root.component';
 
 describe('HomepageRootComponent', () => {
   let spectator: Spectator<HomepageRootComponent>;
+  let component: HomepageRootComponent;
+
   const createComponent = createComponentFactory({
     component: HomepageRootComponent,
     declarations: [HomepageBannerComponent]
   });
 
-  it('should create', () => {
+  beforeEach(() => {
     spectator = createComponent();
+    component = spectator.component;
+  });
 
-    expect(spectator.component).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contain the HomepageBannerComponent', () => {
+    expect(spectator.query('rob-homepage-banner')).toBeTruthy();
   });
 });
