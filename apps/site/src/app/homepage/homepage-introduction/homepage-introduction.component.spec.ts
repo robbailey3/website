27d5fa6dcba7a8b7e7f180aff45834f137dcdee0
Rob Bailey <rob.bailey3@gmail.com>
2021-03-14 20:15:19 +0000

    test: adding tests for homepage components

--- apps/site/src/app/homepage/homepage-introduction/homepage-introduction.component.spec.ts
+++ apps/site/src/app/homepage/homepage-introduction/homepage-introduction.component.spec.ts
@@ -1,26 +1,45 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
 
 import { HomepageIntroductionComponent } from './homepage-introduction.component';
 
-describe('HomepageIntroductionComponent', () => {
-  let component: HomepageIntroductionComponent;
-  let fixture: ComponentFixture<HomepageIntroductionComponent>;
+@Component({
+  selector: 'rob-button',
+  template: `<a [routerLink]="routerLink"><ng-content></ng-content></a>`
+})
+class ButtonStubComponent {
+  @Input() public routerLink;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomepageIntroductionComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
+  @Input() public variant;
+}
+
+describe('HomepageIntroductionComponent', () => {
+  let spectator: Spectator<HomepageIntroductionComponent>;
+  let component: HomepageIntroductionComponent;
+
+  const componentFactory = createComponentFactory({
+    component: HomepageIntroductionComponent,
+    declarations: [ButtonStubComponent],
+    imports: [RouterTestingModule],
+    schemas: [NO_ERRORS_SCHEMA]
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomepageIntroductionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = componentFactory();
+    component = spectator.component;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a button which links to the about page', () => {
+    const button = spectator.query('rob-button');
+
+    const anchor = button.querySelector('a');
+
+    expect(button).toBeDefined();
+    expect(anchor.getAttribute('href')).toEqual('/about');
   });
 });
--- apps/site/src/app/homepage/homepage-root/homepage-root.component.spec.ts
+++ apps/site/src/app/homepage/homepage-root/homepage-root.component.spec.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Spectator, createComponentFactory } from '@ngneat/spectator';
-import * as e from 'express';
 import { HomepageBannerComponent } from '../homepage-banner/homepage-banner.component';
 
 import { HomepageRootComponent } from './homepage-root.component';
@@ -26,5 +25,17 @@ describe('HomepageRootComponent', () => {
 
   it('should contain the HomepageBannerComponent', () => {
     expect(spectator.query('rob-homepage-banner')).toBeTruthy();
+  });
+
+  it('should contain the HomepageIntroductionComponent', () => {
+    expect(spectator.query('rob-homepage-introduction')).toBeTruthy();
+  });
+
+  it('should contain the HomepageCarouselComponent', () => {
+    expect(spectator.query('rob-homepage-carousel')).toBeTruthy();
+  });
+
+  it('should contain the HomepageTechnologiesComponent', () => {
+    expect(spectator.query('rob-homepage-technologies')).toBeTruthy();
   });
 });
