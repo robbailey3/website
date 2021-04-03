8daac57c8c218cb21b8510a7c822f541853dc4e6
Rob Bailey <rob.bailey3@gmail.com>
2021-04-03 20:32:26 +0100

    build: adding footer content

--- apps/site/src/app/global/footer/footer.component.spec.ts
+++ apps/site/src/app/global/footer/footer.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UiComponentsModule } from '@website/ui-components';
 
 import { FooterComponent } from './footer.component';
 
@@ -10,7 +12,7 @@ describe('FooterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule, FontAwesomeModule, UiComponentsModule]
     }).compileComponents();
   });
 
