074d28a0cf453555a108a53a31fcdfae6832dd69
Rob Bailey <rob.bailey3@gmail.com>
2021-04-28 20:09:29 +0100

    build: removing crap i dont want

--- apps/site/src/app/about/about-project/about-project.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'rob-about-project',
-  templateUrl: './about-project.component.html',
-  styleUrls: ['./about-project.component.scss']
-})
-export class AboutProjectComponent {}
--- apps/site/src/app/about/about.module.ts
+++ apps/site/src/app/about/about.module.ts
@@ -5,20 +5,14 @@ import { AboutRootComponent } from './about-root/about-root.component';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutBasicsComponent } from './about-basics/about-basics.component';
 import { AboutTimelineComponent } from './about-timeline/about-timeline.component';
-import { AboutInterestsComponent } from './about-interests/about-interests.component';
-import { AboutProjectComponent } from './about-project/about-project.component';
 import { TimelineItemComponent } from './about-timeline/components/timeline-item/timeline-item.component';
-import { InterestComponent } from './about-interests/interest/interest.component';
 
 @NgModule({
   declarations: [
     AboutRootComponent,
     AboutBasicsComponent,
     AboutTimelineComponent,
-    AboutInterestsComponent,
-    AboutProjectComponent,
     TimelineItemComponent,
-    InterestComponent
   ],
   imports: [CommonModule, AboutRoutingModule, UiComponentsModule]
 })
