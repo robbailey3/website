abb2074bd9ab06d9984d45bb971351c18b19f252
Rob Bailey <rob.bailey3@gmail.com>
2021-05-06 20:15:35 +0100

    fix: fixing broken test

--- apps/site/src/app/shared/directives/code-highlight/code-highlight.directive.spec.ts
+++ apps/site/src/app/shared/directives/code-highlight/code-highlight.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ElementRef } from '@angular/core';
 import { CodeHighlightDirective } from './code-highlight.directive';
 
 describe('CodeHighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new CodeHighlightDirective();
+    const directive = new CodeHighlightDirective(
+      new ElementRef(document.createElement('div'))
+    );
     expect(directive).toBeTruthy();
   });
 });
