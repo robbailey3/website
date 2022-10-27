26e1a9e6c5984502e7ea77bbb58474854d9cf7df
Rob Bailey <rob.bailey3@gmail.com>
2022-10-27 21:20:03 +0100

    Adding pages and under construction

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -3,11 +3,21 @@ import Homepage from './pages/Homepage/Homepage.vue';
 import Login from './pages/auth/Login.vue';
 import Dashboard from './pages/admin/Dashboard.vue';
 import firebase from './services/firebase';
+import BlogPage from '@pages/blog/BlogPage.vue';
+import PhotosPage from '@pages/photos/PhotosPage.vue';
+import CvPage from '@pages/cv/CvPage.vue';
 
 const routes = [
 	{ path: '/', component: Homepage },
 	{ path: '/login', component: Login },
-	{ path: '/admin', component: Dashboard }
+	{ path: '/admin', component: Dashboard },
+	{ path: '/blog', component: BlogPage },
+	{ path: '/photos', component: PhotosPage },
+	{ path: '/cv', component: CvPage },
+	{
+		path: '/projects',
+		component: () => import('@pages/projects/ProjectsPage.vue')
+	}
 ];
 
 export const router = createRouter({
