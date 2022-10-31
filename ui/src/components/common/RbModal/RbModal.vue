e624521c5a0dd4fbc7ceac4e4a1515fc81fc1267
Rob Bailey <rob.bailey3@gmail.com>
2022-10-31 21:29:22 +0000

    Updating modal and adding some tests

--- ui/src/components/common/RbModal/RbModal.vue
+++ ui/src/components/common/RbModal/RbModal.vue
@@ -2,21 +2,26 @@
 	<div v-if="open">
 		<div
 			class="fixed top-0 left-0 w-full h-full bg-slate-50 opacity-5"
+			data-test="backdrop"
 			@click="close"
 		></div>
 		<div
 			class="bg-slate-800 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg overflow-hidden w-1/2 max-w-2xl shadow-xl"
 		>
-			<div class="bg-slate-700 p-4 flex">
+			<div class="bg-slate-600 bg-opacity-5 p-4 flex">
 				<slot name="header"></slot>
 				<div class="ml-auto">
-					<RbIconButton icon="fa-solid fa-xmark" @click="close" />
+					<RbIconButton
+						data-test="close-button"
+						icon="fa-solid fa-xmark"
+						@click="close"
+					/>
 				</div>
 			</div>
 			<div class="p-4">
 				<slot name="content"></slot>
 			</div>
-			<div class="bg-slate-700 p-4 flex justify-end">
+			<div class="bg-slate-600 bg-opacity-5 p-4 flex justify-end">
 				<slot name="buttons"></slot>
 			</div>
 		</div>
