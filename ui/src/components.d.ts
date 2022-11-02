8abc4d2dab143118f560e156eab0b40b50a2e9e5
Rob Bailey <rob.bailey3@gmail.com>
2022-11-02 21:02:54 +0000

    Updating dockerfile

--- ui/src/components.d.ts
+++ ui/src/components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppButton: typeof import('./components/common/Buttons/AppButton/AppButton.vue')['default']
-    AppContainer: typeof import('./components/layout/AppContainer/AppContainer.vue')['default']
     AppHeader: typeof import('./components/global/AppHeader/AppHeader.vue')['default']
     AppNavigation: typeof import('./components/global/AppNavigation/AppNavigation.vue')['default']
     AppNavigationLink: typeof import('./components/global/AppNavigation/AppNavigationLink/AppNavigationLink.vue')['default']
