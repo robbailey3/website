f884ff7e9146d825dbc0b841bc2049b5f5e24211
Rob Bailey <rob.bailey3@gmail.com>
2022-11-02 20:37:48 +0000

    Adding Nav menu mobile mode

--- ui/src/components/global/AppNavigation/AppNavigation.vue
+++ ui/src/components/global/AppNavigation/AppNavigation.vue
@@ -1,14 +1,45 @@
 <template>
-	<div id="app-navigation" class="w-full">
-		<nav class="flex justify-end">
-			<ul class="flex justify-center md:justify-end items-center gap-4 grow">
-				<AppNavigationLink to="/">Home</AppNavigationLink>
-				<AppNavigationLink to="/blog">Blog</AppNavigationLink>
-				<AppNavigationLink to="/photos">Photos</AppNavigationLink>
-				<AppNavigationLink to="/cv">CV</AppNavigationLink>
-				<AppNavigationLink to="/projects">Projects</AppNavigationLink>
-			</ul>
-		</nav>
+	<div
+		id="app-navigation"
+		class="w-full flex justify-end flex-wrap md:flex-nowrap"
+	>
+		<div class="p-2">
+			<RbIconButton
+				icon="fa-solid fa-bars"
+				v-if="isMobile"
+				@click="toggleNavigation"
+			/>
+		</div>
+		<Transition name="bounce">
+			<div
+				v-if="navState === NavState.OPEN"
+				class="md:flex md:justify-end w-full md:w-auto origin-top absolute md:relative left-0 top-full shadow-lg"
+			>
+				<nav
+					class="mx-4 md:mx-0 bg-slate-800 md:bg-transparent bg-opacity-90 rounded-b-lg backdrop-blur-lg"
+				>
+					<ul
+						class="md:flex justify-center md:justify-end items-center gap-4 grow mb-4 md:mb-0"
+					>
+						<AppNavigationLink to="/" @click="toggleNavigation"
+							>Home</AppNavigationLink
+						>
+						<AppNavigationLink to="/blog" @click="toggleNavigation"
+							>Blog</AppNavigationLink
+						>
+						<AppNavigationLink to="/photos" @click="toggleNavigation"
+							>Photos</AppNavigationLink
+						>
+						<AppNavigationLink to="/cv" @click="toggleNavigation"
+							>CV</AppNavigationLink
+						>
+						<AppNavigationLink to="/projects" @click="toggleNavigation"
+							>Projects</AppNavigationLink
+						>
+					</ul>
+				</nav>
+			</div>
+		</Transition>
 	</div>
 </template>
 
@@ -19,16 +50,39 @@ import { NavState } from './navState';
 
 const { width } = useWindowSize();
 
-const isOpen = ref<NavState>(NavState.OPEN);
+const navState = ref<NavState>(NavState.OPEN);
+
+const isMobile = computed(() => width.value <= 768);
 
 onMounted(() => {
-	isOpen.value = width.value > 768 ? NavState.OPEN : NavState.CLOSED;
+	navState.value = width.value > 768 ? NavState.OPEN : NavState.CLOSED;
 });
 
-watch(
-	() => width.value,
-	() => {
-		console.log(width.value);
+const toggleNavigation = () => {
+	if (width.value > 768) {
+		return;
 	}
-);
+	navState.value =
+		navState.value === NavState.CLOSED ? NavState.OPEN : NavState.CLOSED;
+};
 </script>
+
+<style scoped>
+.bounce-enter-active {
+	animation: bounce-in 0.3s ease-in-out;
+}
+.bounce-leave-active {
+	animation: bounce-in 0.3s reverse ease-in-out;
+}
+@keyframes bounce-in {
+	0% {
+		transform: scaleY(0);
+	}
+	50% {
+		transform: scaleY(1.25);
+	}
+	100% {
+		transform: scaleY(1);
+	}
+}
+</style>
--- ui/src/icon-library.ts
+++ ui/src/icon-library.ts
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGithub, faTwitter, faXmark);
+library.add(faGithub, faTwitter, faXmark, faBars);
