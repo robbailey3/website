c5c2ddc5b5f8ff2bc12575191191ead04be6861e
Rob Bailey <rob.bailey3@gmail.com>
2022-12-11 15:22:19 +0000

    Adding axios

--- ui/src/features/auth/AuthActions.ts
+++ ui/src/features/auth/AuthActions.ts
@@ -1,7 +1,12 @@
+import axios from 'axios';
+
 export enum AuthAction {
 	LOGIN = 'LOGIN'
 }
 
-async function GetFirebaseConfig() {}
+async function GetFirebaseConfig() {
+	const result = await axios.get('http://localhost:80/api/config/firebase');
+	const config = result.data;
+}
 
 export async function InitialiseFirebase() {}
