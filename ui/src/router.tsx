93d275dc53592cc37dedd4215add8fd367d446b3
Rob Bailey <rob.bailey3@gmail.com>
2022-12-11 14:36:20 +0000

    Renaming component back

--- ui/src/router.tsx
+++ ui/src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ErrorPage from '@pages/error/ErrorPage';
-import HomePage from '@pages/home/Banana';
+import HomePage from '@pages/home/HomePage';
 import Root from './Root';
 
 const router = createBrowserRouter([
