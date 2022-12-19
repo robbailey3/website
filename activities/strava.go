962a4712ececc91e7c4a0b57976ee84bd5d4fc0f
Rob Bailey <rob.bailey3@gmail.com>
2022-12-19 21:41:54 +0000

    Adding http request for strava

--- activities/strava.go
+++ activities/strava.go
@@ -2,6 +2,10 @@ package activities
 
 import (
   "context"
+  "encoding/json"
+  "fmt"
+  "io"
+  "net/http"
 )
 
 type StravaApiService interface {
@@ -9,9 +13,38 @@ type StravaApiService interface {
 }
 
 type stravaApiService struct {
+  baseUrl string
 }
 
 func (s *stravaApiService) GetActivity(ctx context.Context, id string) (*StravaActivity, error) {
-  // client := &http.Client{}
-  return nil, nil
+  client := &http.Client{}
+  url := fmt.Sprintf("https://www.strava.com/api/v3/activities/%s", id)
+
+  req, err := http.NewRequest("GET", url, nil)
+
+  if err != nil {
+    return nil, err
+  }
+  // TODO: Do something to get auth token
+  req.Header.Add("Authorization", "")
+
+  resp, err := client.Do(req)
+
+  if err != nil {
+    return nil, err
+  }
+
+  var activity StravaActivity
+
+  respStr, err := io.ReadAll(resp.Body)
+
+  if err != nil {
+    return nil, err
+  }
+
+  if err := json.Unmarshal(respStr, &activity); err != nil {
+    return nil, err
+  }
+
+  return &activity, nil
 }
