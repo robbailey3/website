d7db81e97f269b8b766b271d827d585d494b036e
Rob Bailey <rob.bailey3@gmail.com>
2023-01-15 09:39:40 +0000

    Refactoring so tests pass

--- blog/controller_test.go
+++ blog/controller_test.go
@@ -1,6 +1,8 @@
 package blog_test
 
 import (
+  "fmt"
+  "github.com/go-chi/chi/v5"
   "github.com/golang/mock/gomock"
   "github.com/pkg/errors"
   "github.com/robbailey3/website-api/blog"
@@ -11,23 +13,61 @@ import (
   "testing"
 )
 
-func newTestController(service blog.Service) blog.Controller {
-  return blog.NewController(service)
+type TestServer struct {
+  Router         *chi.Mux
+  MockController *gomock.Controller
+  Service        *mock_blog.MockService
+  Controller     blog.Controller
+}
+
+func createNewTestServer(t *testing.T) *TestServer {
+  mockController := gomock.NewController(t)
+  service := mock_blog.NewMockService(mockController)
+  controller := blog.NewController(service)
+  router := chi.NewRouter()
+
+  router.Get("/", controller.GetPosts)
+  router.Get("/{id}", controller.GetPost)
+  router.Patch("/{id}", controller.UpdatePost)
+  router.Post("/", controller.AddPost)
+  router.Delete("/{id}", controller.DeletePost)
+
+  return &TestServer{
+    Router:         router,
+    MockController: mockController,
+    Service:        service,
+    Controller:     controller,
+  }
+}
+
+// checkResponseCode is a simple utility to check the response code
+// of the response
+func checkResponseCode(t *testing.T, expected, actual int) {
+  if expected != actual {
+    t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+  }
+}
+
+// executeRequest, creates a new ResponseRecorder
+// then executes the request by calling ServeHTTP in the router
+// after which the handler writes the response to the response recorder
+// which we can then inspect.
+func executeRequest(req *http.Request, t *TestServer) *httptest.ResponseRecorder {
+  rr := httptest.NewRecorder()
+  t.Router.ServeHTTP(rr, req)
+
+  return rr
 }
 
 func TestController_GetPosts(t *testing.T) {
   t.Run("should return 400 bad request for invalid limit", func(t *testing.T) {
-    mockController := gomock.NewController(t)
-    s := mock_blog.NewMockService(mockController)
+    sut := createNewTestServer(t)
 
-    s.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return([]blog.Post{}, nil)
+    sut.Service.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any())
 
-    c := newTestController(s)
+    request := httptest.NewRequest(http.MethodGet, "/?limit=dodgylimit&offset=0", nil)
 
-    request := httptest.NewRequest(http.MethodGet, "/blog?limit=dodgylimit&offset=0", nil)
-    response := httptest.NewRecorder()
-
-    c.GetPosts(response, request)
+    response := executeRequest(request, sut)
 
     if response.Result().StatusCode != http.StatusBadRequest {
       t.Errorf("incorrect status code returned, got %d", response.Result().StatusCode)
@@ -35,17 +75,13 @@ func TestController_GetPosts(t *testing.T) {
   })
 
   t.Run("should return 400 bad request for invalid offset", func(t *testing.T) {
-    mockController := gomock.NewController(t)
-    s := mock_blog.NewMockService(mockController)
+    sut := createNewTestServer(t)
 
-    s.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return([]blog.Post{}, nil)
+    sut.Service.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return([]blog.Post{}, nil)
 
-    c := newTestController(s)
+    request := httptest.NewRequest(http.MethodGet, "/?limit=100&offset=dodgyoffset", nil)
 
-    request := httptest.NewRequest(http.MethodGet, "/blog?limit=100&offset=dodgyoffset", nil)
-    response := httptest.NewRecorder()
-
-    c.GetPosts(response, request)
+    response := executeRequest(request, sut)
 
     if response.Result().StatusCode != http.StatusBadRequest {
       t.Errorf("incorrect status code returned, got %d", response.Result().StatusCode)
@@ -53,17 +89,13 @@ func TestController_GetPosts(t *testing.T) {
   })
 
   t.Run("should return a status code of 200", func(t *testing.T) {
-    mockController := gomock.NewController(t)
-    s := mock_blog.NewMockService(mockController)
+    sut := createNewTestServer(t)
 
-    s.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return([]blog.Post{}, nil)
+    sut.Service.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return([]blog.Post{}, nil)
 
-    c := newTestController(s)
+    request := httptest.NewRequest(http.MethodGet, "/?limit=100&offset=0", nil)
 
-    request := httptest.NewRequest(http.MethodGet, "/blog?limit=100&offset=0", nil)
-    response := httptest.NewRecorder()
-
-    c.GetPosts(response, request)
+    response := executeRequest(request, sut)
 
     if response.Result().StatusCode != http.StatusOK {
       t.Errorf("incorrect status code returned, got %d", response.Result().StatusCode)
@@ -71,17 +103,13 @@ func TestController_GetPosts(t *testing.T) {
   })
 
   t.Run("should return a 500 status code when the service returns an error", func(t *testing.T) {
-    mockController := gomock.NewController(t)
-    s := mock_blog.NewMockService(mockController)
+    sut := createNewTestServer(t)
 
-    s.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return(nil, errors.New("something went wrong"))
+    sut.Service.EXPECT().GetPosts(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes().Return(nil, errors.New("something went wrong"))
 
-    c := newTestController(s)
+    request := httptest.NewRequest(http.MethodGet, "/?limit=100&offset=0", nil)
 
-    request := httptest.NewRequest(http.MethodGet, "/blog?limit=100&offset=0", nil)
-    response := httptest.NewRecorder()
-
-    c.GetPosts(response, request)
+    response := executeRequest(request, sut)
 
     if response.Result().StatusCode != http.StatusInternalServerError {
       t.Errorf("incorrect status code returned, got %d", response.Result().StatusCode)
@@ -91,20 +119,42 @@ func TestController_GetPosts(t *testing.T) {
 
 func TestController_GetPost(t *testing.T) {
   t.Run("should return 404 when the service returns a not found exception", func(t *testing.T) {
-    mockController := gomock.NewController(t)
-    s := mock_blog.NewMockService(mockController)
+    sut := createNewTestServer(t)
 
-    s.EXPECT().GetPost(gomock.Any(), gomock.Any()).AnyTimes().Return(nil, exception.NotFound())
-
-    c := newTestController(s)
+    sut.Service.EXPECT().GetPost(gomock.Any(), gomock.Any()).AnyTimes().Return(nil, exception.NotFound())
 
     request := httptest.NewRequest(http.MethodGet, "/1234", nil)
-    response := httptest.NewRecorder()
 
-    c.GetPost(response, request)
+    response := executeRequest(request, sut)
 
     if response.Result().StatusCode != http.StatusNotFound {
       t.Errorf("incorrect status code returned, got %d", response.Result().StatusCode)
     }
+  })
+
+  t.Run("should return 500 when the service returns any other exception", func(t *testing.T) {
+    sut := createNewTestServer(t)
+
+    sut.Service.EXPECT().GetPost(gomock.Any(), gomock.Any()).AnyTimes().Return(nil, errors.New("kaboom"))
+
+    request := httptest.NewRequest(http.MethodGet, "/1234", nil)
+
+    response := executeRequest(request, sut)
+
+    if response.Result().StatusCode != http.StatusInternalServerError {
+      t.Errorf("incorrect status code returned, got %d", response.Result().StatusCode)
+    }
+  })
+
+  t.Run("should call the get post method of the service and pass in the Id", func(t *testing.T) {
+    sut := createNewTestServer(t)
+
+    testId := 1234
+
+    request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%d", testId), nil)
+
+    sut.Service.EXPECT().GetPost(gomock.Any(), gomock.Eq(fmt.Sprintf("%d", testId))).AnyTimes().Return(nil, errors.New("kaboom"))
+
+    executeRequest(request, sut)
   })
 }
