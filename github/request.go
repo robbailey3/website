b7b979c25b5706ef85ea9ce210c7ded714496d54
Rob Bailey <rob.bailey3@gmail.com>
2023-02-03 14:05:44 +0000

    Adding some stuff for users

--- github/request.go
+++ github/request.go
@@ -1,9 +1,12 @@
 package github
 
 type GetReposRequest struct {
-  Username  string
-  Sort      string
-  Direction string
-  PerPage   int
-  Page      int
+	Username  string
+	Sort      string
+	Direction string
+	PerPage   int
+	Page      int
+}
+
+type GetUserRequest struct {
 }
