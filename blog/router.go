5369c3a95467371b7ae9e783e1e3e9515162fd70
Rob Bailey <rob.bailey3@gmail.com>
2023-06-01 20:52:32 +0100

    Fix auh

--- blog/router.go
+++ blog/router.go
@@ -2,6 +2,7 @@ package blog
 
 import (
   "github.com/go-chi/chi/v5"
+  "github.com/robbailey3/website-api/middleware"
 )
 
 func SetupBlogRoutes(router chi.Router) {
@@ -13,7 +14,7 @@ func SetupBlogRoutes(router chi.Router) {
     r.Get("/", c.GetPosts)
     r.Get("/{id}", c.GetPost)
     r.Group(func(r chi.Router) {
-      // r.Use(middleware.WithFirebaseAuth)
+      r.Use(middleware.WithFirebaseAuth)
       r.Post("/", c.AddPost)
       r.Patch("/{id}", c.UpdatePost)
       r.Delete("/{id}", c.DeletePost)
