e74a2283c3801d331250eb8496770ed2a88cc3a5
Rob Bailey <rob.bailey3@gmail.com>
2023-06-02 19:36:28 +0100

    Add auth

--- tasks/router.go
+++ tasks/router.go
@@ -2,12 +2,14 @@ package tasks
 
 import (
   "github.com/go-chi/chi/v5"
+  "github.com/robbailey3/website-api/middleware"
 )
 
 func InitTasksRoutes(router chi.Router) {
   controller := NewController()
 
   router.Route("/tasks", func(r chi.Router) {
+    r.Use(middleware.WithFirebaseAuth)
     r.Get("/", controller.GetTasks)
     r.Post("/", controller.CreateTask)
     r.Patch("/{id}", controller.UpdateTask)
